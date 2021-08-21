b90b807324f04638d1829319e8aec60943cc88fb
andrey231102 <73721039+andrey231102@users.noreply.github.com>
2021-08-21 12:47:06 +0300

    1

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public void ApplyDamage(int damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private PlayerMover _mover;
+
+    private void Start()
+    {
+        _mover = GetComponent<PlayerMover>();
+    }
+}
